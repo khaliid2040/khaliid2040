a9af412c111ede8d3b52366661a08497cd2aecd4
khaalid cali <khaliidcaliy@gmil.com>
2023-11-18 11:02:28 +0300

    this commit is for bash.sh script

---
+++ basic.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#creating backup script
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+create_file() {
+    if ! find -name "file*" -type f | grep -q .;
+    then
+        echo "files don't exist, creating files..."
+        sleep 2
+        touch file{1..10}
+
+    else 
+        echo -e "${RED}files already exist${NC}."
+    fi
+}
+create_file
+if find -name "file*" -type f | grep -q .;
+then
+    #$files=$(ls file*)
+    echo "creating archives...."
+    for file in file* 
+    do
+        tar -cf $file.tar $file &>/dev/null && rm file*
+    done
+    echo -e "${GREEN}created successful"
+else
+    echo -e "${RED}failed files doesn't exist${NC}."
+fi
